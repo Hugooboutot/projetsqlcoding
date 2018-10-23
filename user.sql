b0d2a25b9d1a4e7e474a4e550a9e95768cdbce0a
Leo Faucher <leo.faucher@edu.itescia.fr>
2018-10-23 14:12:31 +0200

    update user ddb

--- user.sql
+++ user.sql
@@ -1,30 +1,26 @@
-INSERT INTO user (email, mdp, pseudo, regdate)
-VALUES 	('email01@gmail.com' , '12345'        , 'Mgleizer' , '2018/10/22');
-		('email02@gmail.com' , 'gtgdt12'      , 'Aboucher' , '2018/10/22');
- 		('email03@gmail.com' , 'judegdey'     , 'Hboutot'  , '2018/10/22');
- 		('email04@gmail.com' , 'cbdcvbcbb'    , 'Djisseau' , '2018/10/22');
- 		('email05@gmail.com' , 'hdgycdc'      , 'Lfaucher' , '2018/10/22');
- 		('email06@gmail.com' , 'hdggygef'     , 'Byatera'  , '2018/10/22');
- 		('email07@gmail.com' , 'FGEFFFR'      , 'Jlim'     , '2018/10/22');
- 		('email08@gmail.com' , 'dzhgzdgg'     , 'Bgrandin' , '2018/10/22');
- 		('email09@gmail.com' , 'yagdd'        , 'Dlievre'  , '2018/10/22');
- 		('email10@gmail.com' , 'qfdhsbfqhjs'  , 'Kabdel'   , '2018/10/22');
- 		('email11@gmail.com' , 'djhddhjjd'    , 'Rachour'  , '2018/10/22');
- 		('email12@gmail.com' , 'deddedue'.    , 'Tasloum'  , '2018/10/22');
- 		('email13@gmail.com' , 'fesfedfeddd'  , 'Ntouazi'  , '2018/10/22');
- 		('email14@gmail.com' , 'cehebhfge'    , 'Adrieux'  , '2018/10/22');
- 		('email15@gmail.com' , 'EHBEZKHD'     , 'Aberoule' , '2018/10/22');
- 		('email16@gmail.com' , 'ekuhfheuhf'   , 'Ahuet'    , '2018/10/22');
- 		('email17@gmail.com' , 'JHVFJSVDVVV'  , 'Aaubert'  , '2018/10/22');
- 		('email18@gmail.com' , 'hgjegjggg'    , 'Apulby'   , '2018/10/22');
- 		('email19@gmail.com' , 'EHFBHJEGGGDF' , 'Hadrien'  , '2018/10/22');
- 		('email20@gmail.com' , 'fhvefhjevv'   , 'Barab'    , '2018/10/22');
- 		('email21@gmail.com' , 'FJHFJHS'      , 'Cjoseph'  , '2018/10/22');
- 		('email22@gmail.com' , 'FEKBFGGHHF'   , 'Cmallet'  , '2018/10/22');
- 		('email23@gmail.com' , 'jbfehgSD'     , 'Drussier' , '2018/10/22');
- 		('email24@gmail.com' , 'FSHFBjkffjkk' , 'Dung'     , '2018/10/22');
- 		('email25@gmail.com' , 'FEKHFBKHdgj'  , 'Cheatcode', '2018/10/22');
-
-
-
-
+INSERT INTO user (id, email, mdp, pseudo, regdate)
+VALUES 	(1, 'email01@gmail.com' , '12345'        , 'Mgleizer' , '2018/10/22'),
+		(2, 'email02@gmail.com' , 'gtgdt12'      , 'Aboucher' , '2018/10/22'),
+ 		(3, 'email03@gmail.com' , 'judegdey'     , 'Hboutot'  , '2018/10/22'),
+ 		(4, 'email04@gmail.com' , 'cbdcvbcbb'    , 'Djisseau' , '2018/10/22'),
+ 		(5, 'email05@gmail.com' , 'hdgycdc'      , 'Lfaucher' , '2018/10/22'),
+ 		(6, 'email06@gmail.com' , 'hdggygef'     , 'Byatera'  , '2018/10/22'),
+ 		(7, 'email07@gmail.com' , 'FGEFFFR'      , 'Jlim'     , '2018/10/22'),
+ 		(8, 'email08@gmail.com' , 'dzhgzdgg'     , 'Bgrandin' , '2018/10/22'),
+ 		(9, 'email09@gmail.com' , 'yagdd'        , 'Dlievre'  , '2018/10/22'),
+ 		(10, 'email10@gmail.com' , 'qfdhsbfqhjs'  , 'Kabdel'   , '2018/10/22'),
+ 		(11, 'email11@gmail.com' , 'djhddhjjd'    , 'Rachour'  , '2018/10/22'),
+ 		(12, 'email12@gmail.com' , 'deddedue'    , 'Tasloum'  , '2018/10/22'),
+ 		(13, 'email13@gmail.com' , 'fesfedfeddd'  , 'Ntouazi'  , '2018/10/22'),
+ 		(14, 'email14@gmail.com' , 'cehebhfge'    , 'Adrieux'  , '2018/10/22'),
+ 		(15, 'email15@gmail.com' , 'EHBEZKHD'     , 'Aberoule' , '2018/10/22'),
+ 		(16, 'email16@gmail.com' , 'ekuhfheuhf'   , 'Ahuet'    , '2018/10/22'),
+ 		(17, 'email17@gmail.com' , 'JHVFJSVDVVV'  , 'Aaubert'  , '2018/10/22'),
+ 		(18, 'email18@gmail.com' , 'hgjegjggg'    , 'Apulby'   , '2018/10/22'),
+ 		(19, 'email19@gmail.com' , 'EHFBHJEGGGDF' , 'Hadrien'  , '2018/10/22'),
+ 		(20, 'email20@gmail.com' , 'fhvefhjevv'   , 'Barab'    , '2018/10/22'),
+ 		(21, 'email21@gmail.com' , 'FJHFJHS'      , 'Cjoseph'  , '2018/10/22'),
+ 		(22, 'email22@gmail.com' , 'FEKBFGGHHF'   , 'Cmallet'  , '2018/10/22'),
+ 		(23, 'email23@gmail.com' , 'jbfehgSD'     , 'Drussier' , '2018/10/22'),
+ 		(24, 'email24@gmail.com' , 'FSHFBjkffjkk' , 'Dung'     , '2018/10/22'),
+ 		(25, 'email25@gmail.com' , 'FEKHFBKHdgj'  , 'Cheatcode', '2018/10/22');
