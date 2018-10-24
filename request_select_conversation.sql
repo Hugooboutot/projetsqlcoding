0166c3fc96a033a507da4f1d8878394f1bc52bdc
Squaaw <damien.jisseau@edu.itescia.fr>
2018-10-24 16:58:46 +0200

    Story 8 9 11

--- request_select_conversation.sql
+++ request_select_conversation.sql
@@ -1,20 +1,27 @@
--- Afficher les conversations d'un utilisateur --
+-- Story 8 : Afficher les conversations d'un utilisateur --
 
-SELECT U.pseudo, M.content
-FROM Utilisateur as U
-INNER JOIN Message as M
-		ON U.id = M.id_sender
-WHERE U.id = 1
+SELECT content
+FROM message
+WHERE id_sender = 1
 ORDER BY sendtime DESC;
 
 
--- Afficher les messages d'une conversation --
+-- Story 9 : Afficher les messages d'une conversation --
 
-SELECT U.pseudo, M.content
-FROM Utilisateur as U
-INNER JOIN Message as M
-		ON U.id = M.id_sender
-		AND U.id = M.id_receiver
-WHERE M.id_sender = 1
-AND M.id_receiver = 2
+SELECT content
+FROM message
+WHERE id_sender = 9 AND id_receiver = 10
+OR id_sender = 10 AND id_receiver = 9
 ORDER BY sendtime DESC;
+
+-- Story 11 : Afficher liste services --
+
+SELECT U.pseudo AS Créateur_service, U.telport AS Téléphone_portable, U2.pseudo AS pseudo_inscrit, S.nom, S.description, S.adresse, S.cpost, S.ville, S.pays, S.servicedatetime, S.infoplus
+FROM service AS S
+INNER JOIN user AS U
+	ON U.id = S.id_user
+LEFT JOIN service_user AS SU
+	ON SU.id_user = S.id_user 
+LEFT JOIN user AS U2
+ON  U2.id = SU.id_user
+WHERE S.id = 3;
