c3c12b5c15e302c65cf2934ba9dc5fadb2345281
Leo Faucher <leo.faucher@edu.itescia.fr>
2018-10-24 09:52:44 +0200

    ajout requetes diverses

--- request_create_message.sql
+++ request_create_message.sql
@@ -1,2 +1,2 @@
 INSERT INTO message ( id_sender , id_receiver , content )
-VALUES  ('2', '9', 'pas fait que survivre cinq siècles, mais sest aussi adapté');
+VALUES  (1, 2, 'sample_text');
---
+++ update_user.sql
@@ -0,0 +1,2 @@
+UPDATE user
+SET adresse = "1 rue lambda", cpost = "12345", ville = "ville1", pays = "France", telportable = "0607080910", telfixe = NULL;
